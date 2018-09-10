5e585cfd40a05bdb52ebf3ffd4973120cb8c3196
DocPete <DocPete@users.noreply.github.com>
2018-09-10 10:55:29 -0400

    Adding our Project

---
+++ Assets/TestingGitMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingGitMessage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Testing Git");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
